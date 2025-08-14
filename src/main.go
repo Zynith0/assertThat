b073e1c80be84b4a63e47221228f4e2802c6c188
Zynith0 <nolan.lessard.music@gmail.com>
2025-08-14 13:03:41 -0400

    Added the func contains

--- src/main.go
+++ src/main.go
@@ -15,3 +15,11 @@ func IsEqualTo(t *testing.T, want, got any) {
 		t.Errorf("Wanted %v, but instead got %v", want, got)
 	}
 }
+
+func Contains(t *testing.T, want any, got []any) {
+	for i := 0; i < len(got); i++ {
+		if want != got[i] {
+			t.Errorf("%v does not contain %v", got, want)
+		}
+	}
+}
